bb9eaeeabbaf2698f3613b4c9694b5260431e5d1
MarcellusStack <marcellusstack@gmail.com>
2024-05-06 22:46:07 +0200

    setup project

---
+++ directus-sync.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  directusUrl: "http://localhost:8055", // Change this to your Directus URL
+  directusEmail: "admin@example.com",
+  directusPassword: "Start.123",
+  dumpPath: "./directus-config",
+};
