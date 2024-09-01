08db42764ce9fc0856e75b55a3dc2cf15bab0fd5
MarcellusStack <marcellusstack@gmail.com>
2024-09-01 12:57:36 +0200

    Implement getUser function with caching and Prisma integration
    - introduced a getUser function to fetch user data from the database using Prisma
    - Implemented caching using unstable_cache from next/cache and cache from react to optimize database queries
    - The getUser function retrieves user details, including organization information, based on the user ID
    - The cache is set to revalidate every 60 seconds, ensuring data freshness
    - Defined UserProps type to represent the structure of the user data returned by the getUser function.

---
+++ server/utils/get-user.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { unstable_cache } from "next/cache";
+import { cache } from "react";
+import prisma from "@/lib/prisma";
+
+export const getUser = cache(async (id: string) => {
+  return await unstable_cache(
+    async (id) => {
+      return await prisma.user.findUnique({
+        where: {
+          id: id,
+        },
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          gender: true,
+          birthDate: true,
+          dataPolicy: true,
+          organizationId: true,
+          organization: {
+            select: {
+              adminUserId: true,
+            },
+          },
+        },
+      });
+    },
+    [id],
+    {
+      tags: ["user", id],
+      revalidate: 60,
+    }
+  )(id);
+});
+
+export type UserProps = NonNullable<Awaited<ReturnType<typeof getUser>>>;
