8d0f194f2b302515b72bc4fd765473cb49bd2703
MarcellusStack <marcellusstack@gmail.com>
2024-09-03 21:10:33 +0200

    add get current user
    - for security reasons we added a get current user which is like get user but without passing the id

---
+++ server/utils/get-current-user.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { unstable_cache } from "next/cache";
+import { cache } from "react";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export const getCurrentUser = cache(async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    throw new Error("User not found");
+  }
+
+  return await unstable_cache(
+    async (userId) => {
+      return await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          gender: true,
+          birthDate: true,
+          dataPolicy: true,
+          organizationId: true,
+          organization: {
+            select: {
+              adminUserId: true,
+            },
+          },
+        },
+      });
+    },
+    [userId],
+    {
+      tags: ["user", userId],
+      revalidate: 60,
+    }
+  )(userId);
+});
+
+export type CurrentUserProps = Awaited<ReturnType<typeof getCurrentUser>>;
