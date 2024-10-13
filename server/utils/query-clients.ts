433956a410e07e04a2a90c7ed6b346c9fd24b18e
MarcellusStack <marcellusstack@gmail.com>
2024-10-13 20:09:07 +0200

    refactor authQuery function; simplify structure and ensure non-nullable return type

--- server/utils/query-clients.ts
+++ server/utils/query-clients.ts
@@ -2,21 +2,20 @@ import { auth } from "@clerk/nextjs/server";
 import { CurrentUserProps, getCurrentUser } from "./get-current-user";
 import { checkAdminOrPermission } from "./check-admin-or-permission";
 
-export const authQuery =
-  (permission?: string) => async (): Promise<CurrentUserProps> => {
-    const { userId } = auth();
+export const authQuery = async (permission?: string) => {
+  const { userId } = auth();
 
-    if (!userId) {
-      throw new Error("Bitte melden Sie sich an");
-    }
+  if (!userId) {
+    throw new Error("Bitte melden Sie sich an");
+  }
 
-    const user = await getCurrentUser();
+  const user = await getCurrentUser();
 
-    if (!user) {
-      throw new Error("User not found");
-    }
+  if (!user) {
+    throw new Error("User not found");
+  }
 
-    await checkAdminOrPermission(user, permission);
+  await checkAdminOrPermission(user, permission);
 
-    return user as CurrentUserProps;
-  };
+  return user as NonNullable<CurrentUserProps>;
+};
