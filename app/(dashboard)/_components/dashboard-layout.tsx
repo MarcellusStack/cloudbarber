fe320b46f9ac753b043690416147bdd09dbd7edb
MarcellusStack <MarcellusStack@gmail.com>
2025-09-16 14:10:56 +0200

    feat: enhance DashboardLayout with navigation links and search input; add navlink styles

--- app/(dashboard)/_components/dashboard-layout.tsx
+++ app/(dashboard)/_components/dashboard-layout.tsx
@@ -1,11 +1,31 @@
 "use client";
 import { Breadcrumbs } from "@/components/breadcrumbs";
 import { UserButton } from "@/components/user-button";
-import { AppShell, Burger, Group, rem, TextInput } from "@mantine/core";
+import {
+  AppShell,
+  Burger,
+  Group,
+  NavLink,
+  rem,
+  Stack,
+  TextInput,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { IconSearch } from "@tabler/icons-react";
+import {
+    IconBuildingStore,
+  IconCalendar,
+  IconHome,
+  IconRazorElectric,
+  IconSearch,
+  IconSettings,
+  IconTools,
+  IconUsers,
+  IconUsersGroup,
+} from "@tabler/icons-react";
 import Link from "next/link";
 import React from "react";
+import navLinkStyles from "../navlink.module.css";
+import { usePathname } from "next/navigation";
 
 export const DashboardLayout = ({
   children,
@@ -13,6 +33,8 @@ export const DashboardLayout = ({
   children: React.ReactNode;
 }) => {
   const [opened, { toggle }] = useDisclosure();
+  const pathname = usePathname();
+
   return (
     <AppShell
       layout="alt"
@@ -27,27 +49,107 @@ export const DashboardLayout = ({
         </Group>
       </AppShell.Header>
       <AppShell.Navbar p="md">
-        <Link
-          href="/"
-          style={{
-            textDecoration: "none",
-            fontFamily: "Bebas Neue",
-            fontWeight: 400,
-            fontSize: rem(32),
-            color: "black",
-          }}
-        >
-          CloudBarber
-        </Link>
-        <TextInput
-          leftSection={<IconSearch size={16} />}
-          placeholder="Suche"
-        />
-        <Group>
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-        </Group>
+        <Stack>
+          <Link
+            href="/"
+            style={{
+              textDecoration: "none",
+              fontFamily: "Bebas Neue",
+              fontWeight: 400,
+              fontSize: rem(32),
+              color: "black",
+            }}
+          >
+            CloudBarber
+          </Link>
+          <TextInput
+            mt="-md"
+            leftSection={<IconSearch size={16} />}
+            placeholder="Suche"
+          />
+          <Stack gap="0">
+            <NavLink
+              component={Link}
+              className={navLinkStyles.root}
+              color="primary"
+              href="/dashboard"
+              label="Dashboard"
+              ff={"Roboto"}
+              active={pathname.startsWith("/dashboard")}
+              leftSection={<IconHome size={20} stroke={1.5} />}
+            />
+            <NavLink
+              component={Link}
+              className={navLinkStyles.root}
+              color="primary"
+              href="/services"
+              label="Dienstleistungen"
+              ff={"Roboto"}
+              active={pathname.startsWith("/services")}
+              leftSection={<IconRazorElectric size={20} stroke={1.5} />}
+            />
+            <NavLink
+              component={Link}
+              className={navLinkStyles.root}
+              color="primary"
+              href="/employees"
+              label="Mitarbeiter"
+              ff={"Roboto"}
+              active={pathname.startsWith("/employees")}
+              leftSection={<IconUsers size={20} stroke={1.5} />}
+            />
+            <NavLink
+              component={Link}
+              className={navLinkStyles.root}
+              color="primary"
+              href="/calendar"
+              label="Kalender"
+              ff={"Roboto"}
+              active={pathname.startsWith("/calendar")}
+              leftSection={<IconCalendar size={20} stroke={1.5} />}
+            />
+            <NavLink
+              component={Link}
+              className={navLinkStyles.root}
+              color="primary"
+              href="/customers"
+              label="Kunden"
+              ff={"Roboto"}
+              active={pathname.startsWith("/customers")}
+              leftSection={<IconUsersGroup size={20} stroke={1.5} />}
+            />
+            <NavLink
+              component={Link}
+              className={navLinkStyles.root}
+              color="primary"
+              href="/shop"
+              label="Shop-Seite"
+              ff={"Roboto"}
+              active={pathname.startsWith("/shop")}
+              leftSection={<IconBuildingStore size={20} stroke={1.5} />}
+            />
+            <NavLink
+              component={Link}
+              className={navLinkStyles.root}
+              color="primary"
+              href="/settings"
+              label="Einstellungen"
+              ff={"Roboto"}
+              active={pathname.startsWith("/settings")}
+              leftSection={<IconSettings size={20} stroke={1.5} />}
+            />
+          </Stack>
+          <Group>
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+          </Group>
+        </Stack>
       </AppShell.Navbar>
-      <AppShell.Main>{children}</AppShell.Main>
+      <AppShell.Main bg="quaternary.0">{children}</AppShell.Main>
     </AppShell>
   );
 };
---
+++ app/(dashboard)/services/page.tsx
@@ -0,0 +1,10 @@
+import { SectionHeading } from "@/components/section-heading";
+
+export default function Page() {
+  return (
+    <SectionHeading
+      title="Dienstleistungen"
+      description="Verwalten Sie Ihre Dienstleistungen"
+    />
+  );
+}
